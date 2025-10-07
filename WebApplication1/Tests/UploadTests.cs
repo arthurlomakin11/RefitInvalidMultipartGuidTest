e55431c8d6be80653587a5f50398c3cf920cd827
Artur Lomakin <artur.lomakin@sparkybit.com>
2025-10-07 11:38:24 +0300

    Temporary ContentSerializer Guid fix for Refit

--- WebApplication1/Tests/UploadTests.cs
+++ WebApplication1/Tests/UploadTests.cs
@@ -1,10 +1,38 @@
 ﻿using System.Net;
+using System.Reflection;
+using System.Text;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Refit;
 using WebApplication1.Interfaces;
 using Xunit;
 
 namespace WebApplication1.Tests;
+
+public class GuidFriendlyHttpContentSerializer(IHttpContentSerializer inner)
+    : IHttpContentSerializer
+{
+    public HttpContent ToHttpContent<T>(T item)
+    {
+        if (item is Guid guid)
+        {
+            return new StringContent(guid.ToString(), Encoding.UTF8, "text/plain");
+        }
+
+        return inner.ToHttpContent(item);
+    }
+
+    public async Task<T?> FromHttpContentAsync<T>(
+        HttpContent content,
+        CancellationToken cancellationToken = default)
+    {
+        return await inner.FromHttpContentAsync<T>(content, cancellationToken);
+    }
+
+    public string? GetFieldNameForProperty(PropertyInfo propertyInfo)
+    {
+        return inner.GetFieldNameForProperty(propertyInfo);
+    }
+}
 
 public class UploadTests : IClassFixture<WebApplicationFactory<Program>>
 {
@@ -16,7 +44,10 @@ public class UploadTests : IClassFixture<WebApplicationFactory<Program>>
         
         var httpClient = factory.CreateDefaultClient(handler);
         
-        _client = RestService.For<IUploadApi>(httpClient);
+        _client = RestService.For<IUploadApi>(httpClient, new RefitSettings
+        {
+            ContentSerializer = new GuidFriendlyHttpContentSerializer(new SystemTextJsonContentSerializer())
+        });
     }
 
     [Fact]
